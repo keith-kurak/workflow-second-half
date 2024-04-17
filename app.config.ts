6781223b6e1ec52fe18777e869bf2d8485dc5121
Keith Kurak <keith.kurak@gmail.com>
2024-04-16 23:44:19 -0400

    add widget stubs

---
+++ app.config.ts
@@ -0,0 +1,57 @@
+import "ts-node/register";
+import { ExpoConfig } from "expo/config";
+
+module.exports = ({ config }: { config: ExpoConfig }) => {
+  return {
+    name: "workflow-second-half",
+    slug: "workflow-second-half",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./assets/images/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.keithkurak.workflowsecondhalf",
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      package: "com.keithkurak.workflowsecondhalf",
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-quick-actions",
+        {
+          androidIcons: {
+            fav_icon: {
+              foregroundImage: "./assets/images/adaptive-icon-fav.png",
+              backgroundColor: "#ffffff",
+            },
+          },
+          iosIcons: {
+            fav_icon: "./assets/images/fav.png",
+          },
+        },
+      ],
+      ["./plugins/withWidget.ts"]
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+  };
+};
