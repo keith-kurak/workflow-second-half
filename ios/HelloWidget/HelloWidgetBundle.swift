cd1c64c13c32f823738b6841e9d3531053ce9bc3
Keith Kurak <keith.kurak@gmail.com>
2024-04-17 03:30:47 -0400

    ios widget manual setup

---
+++ ios/HelloWidget/HelloWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  HelloWidgetBundle.swift
+//  HelloWidget
+//
+//  Created by Keith on 4/17/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct HelloWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        HelloWidget()
+    }
+}
